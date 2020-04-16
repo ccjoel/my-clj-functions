150e78ab3c6ae07d791d000ed5c7d767b1ff54fa
Joel Quiles <quilesbaker@gmail.com>
2020-04-15 22:41:23 -0400

    cleaner impl of string = 100

--- src/my_clj_snippets/hundred.clj
+++ src/my_clj_snippets/hundred.clj
@@ -1,9 +1,6 @@
 (ns my-clj-snippets.hundred
   (:require [clojure.string :refer [replace split join]]))
 
-(def worse [1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9])
-(def worse-n [1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9])
-
 (defn parse
   "Parses a vector equation to result number
    Sample input: [123 '+ 4 '- 5 '+ 67 '- 89] ; = 100 ; output"
@@ -26,11 +23,11 @@
 
 (defn char->int
   [i]
-  (Integer/parseInt (str i)))
+  (Long/parseLong (str i)))
 
 (defn concat-numbers
   [a b]
-  (Integer/parseInt (str a b)))
+  (Long/parseLong (str a b)))
 
 (defn split-seq [num-seq-string]
   (let [[first-char & rest-chars] num-seq-string
@@ -48,15 +45,13 @@
     [num]
     (conj (pop v) (concat-numbers (peek v) num))))
 
-(defn gen-equation
-
+(defn ^:generic gen-equation
   "Given a number string and a result, inserts + or - anywhere to generate an equation for result.
-   Prints output of all equations that equal desired result.
+   Returns vector of all expressions that equal desired result.
 
    Example inputs: \"123456789\" and 100
-   Sample printed output: 123 + 4 - 5 + 67 - 89
-   (which equals to 100)"
-
+   Sample item from output: 123 + 4 - 5 + 67 - 89; output is a vector of possibly many expressions, where 1 of
+   them is the above sample, which equals 100"
   ([^java.lang.String num-string
     ^java.lang.Long desired-result]
    (gen-equation num-string desired-result []))
@@ -65,19 +60,16 @@
    ;; end condition
    (if (empty? num-string) ;; works for both empty coll and empty str
      (when (= (parse acc) desired-result)
-       (apply print acc)
-       (print "\n"))
+       [acc])
      ;; else, branch out to 1, -1, next-num, + or -
      (let [[next-num queue] (split-seq num-string)
            branches (if (empty? acc)
                       [[next-num] [(* -1 next-num)]]
                       [(merge-last-num acc next-num) (conj acc '+ next-num) (conj acc '- next-num)])]
-       (doseq [branch branches]
-         (gen-equation queue desired-result branch))))))
 
-#_(join " " [123 '+ 4 '- 5 '+ 67 '- 89])
+       (mapcat #(gen-equation queue desired-result %) branches)))))
 
-(defn sum-to-100
+(defn ^:main sum-to-100
   "Programming exercise solution:
   Insert the mathematical operators + or - before any of the digits in the
   decimal numeric string 123456789
@@ -86,21 +78,12 @@
 
   Yields all possible combinations."
   []
-  (gen-equation "123456789" 100))
+  (->> 100                             ;; See unit test for output
+       (gen-equation "123456789")
+       (map #(join " " %))))
 
-(comment "For the curious, calling (sum-to-100 yields:
+;; =================================== COMMENTS ======================================
 
-123 + 45 - 67 + 8 - 9
-123 + 4 - 5 + 67 - 89
-123 - 45 - 67 + 89
-123 - 4 - 5 - 6 - 7 + 8 - 9
-12 + 3 + 4 + 5 - 6 - 7 + 89
-12 + 3 - 4 + 5 + 67 + 8 + 9
-12 - 3 - 4 + 5 - 6 + 7 + 89
-1 + 23 - 4 + 56 + 7 + 8 + 9
-1 + 23 - 4 + 5 + 6 + 78 - 9
-1 + 2 + 34 - 5 + 67 - 8 + 9
-1 + 2 + 3 - 4 + 5 + 6 + 78 + 9
--1 + 2 - 3 + 4 + 5 + 6 + 78 + 9
-
-")
+(comment
+  (def worse [1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9])
+  (def worse-n [1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9]))
